9f41b6c5c44cea9806efd95f5f94927f51a74e02
KOO JIN WOOK <45314322+JINUKK@users.noreply.github.com>
2025-09-13 21:39:34 +0900

    [#8] Feat: WebClient 의존성 추가 및 WebClientAdapter 클래스 생성 (#9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+	implementation("org.springframework.boot:spring-boot-starter-webflux:3.5.4")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
