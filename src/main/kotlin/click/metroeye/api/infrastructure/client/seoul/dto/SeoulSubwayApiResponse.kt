99ae52dbf9c05bc5f2292471b8eb8aecbf0a7d3f
JINUKK <askjw123@naver.com>
2025-11-23 22:53:52 +0900

    [#54] Refactor: WebClient 비동기(Mono) 기반 처리 변경

--- src/main/kotlin/click/metroeye/api/infrastructure/client/seoul/dto/SeoulSubwayApiResponse.kt
+++ src/main/kotlin/click/metroeye/api/infrastructure/client/seoul/dto/SeoulSubwayApiResponse.kt
@@ -2,7 +2,7 @@ package click.metroeye.api.infrastructure.client.seoul.dto
 
 import org.apache.logging.log4j.message.Message
 
-data class SeoulSubwayApiResponse<T>(
+data class SeoulSubwayApiResponse<out T>(
     val success: Boolean,
     val message: String,
     val data: T?
