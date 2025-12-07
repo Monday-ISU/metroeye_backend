35d515bdf3da061132cf15b3bcb161f59fc5128c
JINUKK <askjw123@naver.com>
2025-12-07 18:48:41 +0900

    [#60] Refactor: WebClient 설정 클래스 분리

--- src/main/kotlin/click/metroeye/api/infrastructure/client/common/WebClientAdapter.kt
+++ src/main/kotlin/click/metroeye/api/infrastructure/client/common/WebClientAdapter.kt
@@ -3,23 +3,17 @@ package click.metroeye.api.infrastructure.client.common
 import org.slf4j.LoggerFactory
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
 
 @Component
 class WebClientAdapter(
-    webClientBuilder: WebClient.Builder
+    private val webClient: WebClient
 ) {
     companion object {
         private val logger = LoggerFactory.getLogger(WebClientAdapter::class.java)
     }
-
-    private val webClient: WebClient = webClientBuilder
-        .filter(requestFilter())
-        .filter(responseFilter())
-        .build()
 
     fun <T> get(
         uri: String,
@@ -37,49 +31,9 @@ class WebClientAdapter(
             return Mono.empty()
         }
 
-        return this.webClient.get()
+        return webClient.get()
             .uri(requestUri)
             .retrieve()
             .bodyToMono(responseType)
-            .onErrorResume {
-                logger.error("""
-                    
-                    
-                    |[WEBCLIENT REQUEST ERROR]
-                    |>> METHOD: GET
-                    |>> MESSAGE: ${it.message}
-                """.trimIndent()
-                )
-                Mono.empty()
-            }
-    }
-
-    private fun requestFilter(): ExchangeFilterFunction {
-        return ExchangeFilterFunction.ofRequestProcessor {
-            request ->
-            logger.info("""
-                
-                
-                |[WEBCLIENT REQUEST]
-                |>> METHOD: ${request.method()}
-                |>> URL: ${request.url()} 
-                """.trimIndent()
-            )
-            Mono.just(request)
-        }
-    }
-
-    private fun responseFilter(): ExchangeFilterFunction {
-        return ExchangeFilterFunction.ofResponseProcessor {
-                response ->
-            logger.info("""
-                
-                
-                |[WEBCLIENT RESONSE]
-                |>> STATUS: ${response.statusCode()}
-                """.trimIndent()
-            )
-            Mono.just(response)
-        }
     }
 }
