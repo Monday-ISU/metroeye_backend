081065ee1fe28fcc212d42b8c8e200b0fd93ad03
JINUKK <askjw123@naver.com>
2026-01-19 00:05:35 +0900

    [#75] Feat: 요청 및 응답 로깅 필터 클래스 생성

--- src/main/kotlin/click/metroeye/api/exception/ApiExceptionHandler.kt
+++ src/main/kotlin/click/metroeye/api/exception/ApiExceptionHandler.kt
@@ -1,20 +1,37 @@
 package click.metroeye.api.exception
 
 import click.metroeye.api.presentation.v1.dto.response.ApiResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice(basePackages = ["click.metroeye.api.presentation.v1.controller"])
 class ApiExceptionHandler {
-    @ExceptionHandler(value = [
-        ApiException::class
-    ])
-    fun apiException(apiException: ApiException): ResponseEntity<ApiResponse<Void?>> {
-        return ResponseEntity.ok(ApiResponse(
-            apiException.clientMessage,
-            apiException.serverMessage,
-            null
-        ))
+    @ExceptionHandler(Exception::class)
+    fun exception(e: Exception): ResponseEntity<ApiResponse<Void?>> {
+        return when (e) {
+            is ApiException -> {
+                ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(
+                        ApiResponse(
+                            e.clientMessage,
+                            e.serverMessage,
+                            null
+                        )
+                    )
+            }
+
+            else -> {
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(
+                        ApiResponse(
+                            "서버 오류가 발생했습니다.",
+                            e.message ?: "UNKNOWN SERVER ERROR",
+                            null
+                        )
+                    )
+            }
+        }
     }
 }
---
+++ src/main/kotlin/click/metroeye/api/filter/LogFilter.kt
@@ -0,0 +1,48 @@
+package click.metroeye.api.filter
+
+import click.metroeye.api.decorator.CachedContentServerHttpResponse
+import click.metroeye.api.util.RequestHeaderUtils
+import org.slf4j.LoggerFactory
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+class LogFilter : WebFilter {
+    companion object {
+        private val logger = LoggerFactory.getLogger(LogFilter::class.java)
+    }
+
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void?> {
+        logger.info("""
+            
+            
+            |[REQUEST]
+            |>> METHOD: ${exchange.request.method}
+            |>> REQUEST URI: ${exchange.request.uri.path}
+            |>> CLIENT IP: ${RequestHeaderUtils.getClientIp(exchange.request)}
+            |>> HEADERS: ${RequestHeaderUtils.getStringHeaders(exchange.request)}
+            |>> REQUEST PARAM: ${exchange.request.queryParams}
+            """.trimIndent()
+        )
+
+        val cachingResponse = CachedContentServerHttpResponse(exchange.response)
+
+        val mutatedExchange = exchange.mutate()
+            .response(cachingResponse)
+            .build()
+
+        return chain.filter(mutatedExchange)
+            .doFinally {
+                val status = exchange.response.statusCode?.value() ?: 200
+
+                logger.info("""
+            
+            
+                    |[RESPONSE]
+                    |>> STATUS: $status
+                    ${if (status != 200) "|>> RESPONSE BODY: ${cachingResponse.getCachedContent()}" else ""}   
+                    """.trimIndent())
+            }
+    }
+}
