b2f887013399b1aeee918d71456e1d4296b7eb30
JINUKK <askjw123@naver.com>
2026-04-05 23:54:18 +0900

    [#109] Fix: lines 테이블명 백틱 적용

--- src/main/kotlin/click/metroeye/api/infrastructure/persistence/entity/LineEntity.kt
+++ src/main/kotlin/click/metroeye/api/infrastructure/persistence/entity/LineEntity.kt
@@ -6,7 +6,7 @@ import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
-@Table("lines")
+@Table("`lines`")
 data class LineEntity(
     @Id
     @Column("id")
