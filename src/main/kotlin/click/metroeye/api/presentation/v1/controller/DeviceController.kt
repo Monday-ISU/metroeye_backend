d138bb482049ec9d64548879b9846356efcc3664
JINUKK <askjw123@naver.com>
2026-05-05 14:32:34 +0900

    [#114] Docs: Swagger 문서 응답 예시 고도화

--- src/main/kotlin/click/metroeye/api/presentation/v1/controller/DeviceController.kt
+++ src/main/kotlin/click/metroeye/api/presentation/v1/controller/DeviceController.kt
@@ -1,11 +1,14 @@
 package click.metroeye.api.presentation.v1.controller
 
-import click.metroeye.api.application.service.DeviceService
 import click.metroeye.api.application.dto.CreateDeviceRequestModel
+import click.metroeye.api.application.service.DeviceService
 import click.metroeye.api.presentation.v1.dto.request.CreateDeviceRequest
 import click.metroeye.api.presentation.v1.dto.response.ApiResponse
 import click.metroeye.api.presentation.v1.dto.response.CreateDeviceResponse
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -31,6 +34,66 @@ class DeviceController(
 - **uuid**: 기기 고유 번호
 - **osType**: OS 유형 (ANDROID, IOS)
 """
+    )
+    @ApiResponses(
+        value = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        examples = [
+                            ExampleObject(
+                                value = """
+                                    {
+                                        "clientMessage": "등록되었습니다.",
+                                        "serverMessage": "SUCCESS",
+                                        "data": {
+                                            "secret": "generated-secret-key",
+                                            "accessToken": "json-web-token",
+                                            "refreshToken": "json-web-token",
+                                            "expiresIn": 1800
+                                        }
+                                    }
+                                """
+                            ),
+                        ]
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "400",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        examples = [
+                            ExampleObject(
+                                name = "기기 고유 번호 누락",
+                                description = "필수 요청 값인 기기 고유 번호(uuid)가 누락된 경우 발생합니다.",
+                                value = """
+                                    {
+                                        "clientMessage": "기기 고유 번호가 누락되었습니다.",
+                                        "serverMessage": "CreateDeviceRequestModel.uuid is blank.",
+                                        "data": null
+                                    }
+                                """
+                            ),
+                            ExampleObject(
+                                name = "지원하지 않는 OS 유형",
+                                description = "요청한 OS 유형이 ANDROID 또는 IOS가 아닌 경우 발생합니다.",
+                                value = """
+                                    {
+                                        "clientMessage": "지원하지 않는 OS 유형입니다.",
+                                        "serverMessage": "CreateDeviceRequestModel.osType is invalid.",
+                                        "data": null
+                                    }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
     )
     @PostMapping
     fun createDevice(
