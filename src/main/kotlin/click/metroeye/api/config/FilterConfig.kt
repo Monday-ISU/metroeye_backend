5ae27fe3e974353c9d5dee02429d877f07322329
KOO JIN WOOK <45314322+jinwookkoo@users.noreply.github.com>
2026-05-09 23:33:07 +0900

    [#128] Refactor: LogFilter 최우선 순서 적용 (#129)

--- src/main/kotlin/click/metroeye/api/config/FilterConfig.kt
+++ src/main/kotlin/click/metroeye/api/config/FilterConfig.kt
@@ -3,10 +3,13 @@ package click.metroeye.api.config
 import click.metroeye.api.filter.LogFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.web.server.WebFilter
 
 @Configuration
 class FilterConfig {
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     fun logFilter(): WebFilter = LogFilter()
 }
