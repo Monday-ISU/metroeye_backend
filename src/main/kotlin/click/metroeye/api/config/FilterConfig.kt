294e404a61b8e4c740e1f4a66ce0ab621d4ad4a8
JINUKK <askjw123@naver.com>
2026-05-30 21:08:28 +0900

    [#140] Feat: 서버 점검 및 강제 업데이트 기능 생성

--- src/main/kotlin/click/metroeye/api/config/FilterConfig.kt
+++ src/main/kotlin/click/metroeye/api/config/FilterConfig.kt
@@ -1,6 +1,9 @@
 package click.metroeye.api.config
 
+import click.metroeye.api.filter.ApiFilter
 import click.metroeye.api.filter.LogFilter
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
@@ -12,4 +15,11 @@ class FilterConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     fun logFilter(): WebFilter = LogFilter()
+
+    @Bean
+    fun apiFilter(
+        @Value("\${app.min-version}") minVersion: String,
+        @Value("\${app.maintenance}") isMaintenance: Boolean,
+        objectMapper: ObjectMapper
+    ): WebFilter = ApiFilter(minVersion, isMaintenance, objectMapper)
 }
